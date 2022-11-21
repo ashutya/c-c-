ecd58e304bcbec82109149e75197b83afd5f08e3
ashutya <118053256+ashutya@users.noreply.github.com>
2022-11-21 21:39:31 +0530

    Add files via upload

---
+++ positivecheck.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a;
+cout<<"\n enter number to check";
+cin>>a;
+if(a>0)
+cout<<"no is positive";
+else if(a<0)
+cout<<"no is negative";
+else
+cout<<"no is zero";
+return(0);
+}
