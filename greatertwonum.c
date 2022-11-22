69be9739bbd1380ffd0dfb47b8840508658f5c2d
ashutya <118053256+ashutya@users.noreply.github.com>
2022-11-22 19:07:24 +0530

    Add files via upload

---
+++ greatertwonum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("enter two number");
+    scanf("%d%d",&a,&b);
+    if(a>b)
+    {
+        printf("a is greater");
+    }
+    else
+    printf("b is greater");
+}
