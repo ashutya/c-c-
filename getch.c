b4519124c90c1b31c1cb249583f8d202f3f17a4a
ashutya <118053256+ashutya@users.noreply.github.com>
2022-12-04 15:54:06 +0530

    Add files via upload

---
+++ getch.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char ch,val;
+    ch=getch();
+    printf("ch=%c",ch);
+    printf("\n");
+    val=getche();
+    printf(" val=%c",val);
+}
