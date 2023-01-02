31a0c9e17c2f0173561bb4f6ca4c2df61c01f00e
Ashu Tyagi <118053256+ashutya@users.noreply.github.com>
2023-01-02 22:11:55 +0530

    Add files via upload

--- arraymax.c
+++ arraymax.c
@@ -1,17 +1,28 @@
 #include<stdio.h>
-int max(int a[]);
+int findmaximum(int a[]);
+int findminimum(int a[]);
+int findsumofarray(int a[]);
 void main()
 {
-    int a[10],i;
+    int a[10],max,i;
     printf("enter element in array");
     for(i=0;i<10;i++)
     {
         scanf("%d",&a[i]);
     }
-   max(a);
-   printf("max=%d",max(a));
+    printf("array element are\n");
+    for(i=0;i<10;i++)
+    {
+        printf("%d\n",a[i]);
+    }
+  max=findmaximum(a);
+   printf("max element=%d\n",max);
+   //findminimum(a);
+   printf("min element=%d\n",findminimum(a));
+  // findsumofarray(a);
+   printf("sum of element  = %d",findsumofarray(a));
 }
-int max(int a[])
+int findmaximum(int a[])
 {
     int max=a[0],i;
 for(i=1;i<10;i++)
@@ -20,4 +31,23 @@ for(i=1;i<10;i++)
     max=a[i];
 }
 return max;
+}
+int findminimum(int a[])
+{
+    int min=a[0],i;
+for(i=1;i<10;i++)
+{
+    if(min>a[i])
+    min=a[i];
+}
+return min;
+}
+int findsumofarray(int a[])
+{
+    int sum=0,i;
+    for(i=0;i<10;i++)
+    {
+        sum=sum+a[i];
+    }
+    return sum;
 }
